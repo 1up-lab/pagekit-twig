a838a22b025388f558373a615d3e1688956068e6
Jim Schmid <jim.schmid@1up.io>
2014-07-25 17:07:35 +0200

    A bit of cleanup towards nice documented code.

--- src/Provider/TwigProvider.php
+++ src/Provider/TwigProvider.php
@@ -7,15 +7,26 @@ use Oneup\PagekitTwig\Twig\Engine;
 use Pagekit\Framework\Application;
 use Pagekit\Framework\ServiceProviderInterface;
 use Pagekit\Framework\Templating\TemplateNameParser;
+use Symfony\Component\Templating\DelegatingEngine;
 use Twig_Environment;
 
 class TwigProvider implements ServiceProviderInterface
 {
+    /**
+     * Register the Twig environment.
+     *
+     * Not only is the twig environment created, but it is
+     * also registered in the delegating engine. Furthermore
+     * the extension .twig is registered, so the parser
+     * knows that this is a supported format.
+     *
+     * @param Application $app
+     */
     public function register(Application $app)
     {
         $loader = new Loader($app['tmpl.parser']);
         $twig = new Twig_Environment($loader, [
-            'cache' => $app['path'] . '/app/cache/templates',
+            'cache' => $app['path'] . '/app/cache/templates/twig',
             'debug' => true
         ]);
 
@@ -23,11 +34,12 @@ class TwigProvider implements ServiceProviderInterface
         $parser = $app['tmpl.parser'];
         $parser->addEngine('twig', '.twig');
 
-        $app['tmpl']->addEngine(new Engine($twig, $app['tmpl.parser']));
+        /** @var DelegatingEngine $delegatingEngine */
+        $delegatingEngine = $app['tmpl'];
+        $delegatingEngine->addEngine(new Engine($twig, $app['tmpl.parser']));
     }
 
     public function boot(Application $app)
     {
-        // will this ever be called ?
     }
 }
