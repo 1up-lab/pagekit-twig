49844384c7433d8b33b83bea6cdcb69db97b6e6d
Jim Schmid <jim.schmid@1up.io>
2014-07-25 17:28:15 +0200

    New logo file.

--- extension.php
+++ extension.php
@@ -4,14 +4,5 @@ return [
     'main' => 'Oneup\\PagekitTwig\\TwigExtension',
     'autoload' => [
         'Oneup\\PagekitTwig\\' => 'src'
-    ],
-    'menu' => [
-        'hello' => [
-            'label'  => 'Twig',
-            'icon'   => 'extension://twig/extension.svg',
-            'url'    => '@twig/twig',
-            'active' => '@twig/twig*',
-            'access' => 'twig: manage twigs' // wtf?
-        ]
     ]
 ];
